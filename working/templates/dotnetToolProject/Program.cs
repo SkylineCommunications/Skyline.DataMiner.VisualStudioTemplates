10ccb0325be5e4ba8a73d10834780203e94212c3
Jan Staelens <jan.staelens@skyline.be>
2023-08-09 09:42:34 +0200

    dotnet tool templates

---
+++ working/templates/dotnetToolProject/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace $PackageId$
+{
+    using System.CommandLine;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Checks what projects are Legacy style or SDK Style.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// $PackageDescription$.
+        /// </summary>
+        /// <param name="args">Extra arguments.</param>
+        /// <returns>0 if successful.</returns>
+        public static async Task<int> Main(string[] args)
+        {
+            var exampleArgument = new Option<string>(
+                name: "--exampleArgument",
+                description: "Just an example argument.")
+            {
+                IsRequired = true
+            };
+
+            var rootCommand = new RootCommand("$PackageDescription$")
+            {
+                exampleArgument,
+            };
+
+            rootCommand.SetHandler(Process, exampleArgument);
+
+            await rootCommand.InvokeAsync(args);
+
+            return 0;
+        }
+
+        private static async Task Process(string exampleArgument)
+        {
+            //Main Code for program here
+        }
+    }
+}
