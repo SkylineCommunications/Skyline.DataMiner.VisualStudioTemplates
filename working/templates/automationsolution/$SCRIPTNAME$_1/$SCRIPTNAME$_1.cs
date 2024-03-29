3698de15c008e6898a5bc3e3a1493a6d6515bd99
Blake Smethers <108884880+BlakeSLC@users.noreply.github.com>
2024-03-29 08:20:29 -0400

    Add Try/Catch to Automation Script Solution Template (#18)
    * Update $SCRIPTNAME$_1.cs
    
    Example try catch block inserted into template
    
    * Update $SCRIPTNAME$_1.cs
    
    Updated with suggestions from Thomas
    
    * Update $SCRIPTNAME$_1.cs
    
    A&O feedback updates:
    added just the ScriptTimeoutException catch as suggested and updated some of the comments
    
    (cherry picked from commit c82c060c5984ee6024291dc54168f6f3cc076682)

--- working/templates/automationsolution/$SCRIPTNAME$_1/$SCRIPTNAME$_1.cs
+++ working/templates/automationsolution/$SCRIPTNAME$_1/$SCRIPTNAME$_1.cs
@@ -68,7 +68,40 @@ namespace $NAMESPACE$_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+			try
+			{
+				RunSafe(engine);
+			}
+			catch (ScriptAbortException)
+			{
+				// catch normal abort exceptions (engine.ExitFail or engine.ExitSuccess)
+				throw; // Comment if it should be treated as a normal exit of the script.
+			}
+			catch (ScriptForceAbortException)
+			{
+				// catch forced abort exceptions, caused via external maintenance messages
+				throw;
+			}
+			catch (ScriptTimeoutException)
+			{
+				// catch timeout exceptions for when a script has been running for too long
+				throw;
+			}
+			catch (InteractiveUserDetachedException)
+			{
+				// catch a user detaching from the interactive script by closing the window
+				// only applicable for interactive scripts, can be removed for non-interactive scripts.
+				throw;
+			}
+			catch (Exception e)
+			{
+				engine.ExitFail("Run|Something went wrong: " + e);
+			}
+		}
+
+		private void RunSafe(IEngine engine)
+		{
+			// TODO: Define code here
 		}
 	}
 }
