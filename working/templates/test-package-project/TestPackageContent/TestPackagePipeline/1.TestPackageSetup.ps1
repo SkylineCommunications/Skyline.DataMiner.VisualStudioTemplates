57f346a970d9e7648de11f32b91e6332443d1390
Michiel Oda <michiel.oda@skyline.be>
2026-03-05 09:24:11 +0100

    Add QAOps Test Package

---
+++ working/templates/test-package-project/TestPackageContent/TestPackagePipeline/1.TestPackageSetup.ps1
@@ -0,0 +1,22 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]$PathToTestPackageContent
+)
+
+$ErrorActionPreference = 'Stop'
+
+# Import common code module
+Import-Module -Name (Join-Path $PSScriptRoot 'CommonCode.psm1')
+
+try {
+    Write-Host "Running Test Package setup..." -ForegroundColor Cyan
+
+    <#
+        This is a placeholder for where the test setup logic would go.
+        This could include preparing test data, configuring environments, etc.
+    #>
+}
+catch {
+    Write-Error $_.Exception.Message
+    exit 1
+}
